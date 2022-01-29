22f97bd295e6862caef6e7cef69451298ca62b92
Yogesh Burman <82220086+yogeshburman@users.noreply.github.com>
2022-01-29 20:14:15 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,28 @@
+class Test {
+  
+  private int accountno;
+  
+  // setter method 
+  public void setAccountno(int no){
+    
+    accountno =no;
+  }
+  
+  //getter method 
+  public int  getAccountno(){
+    
+    return accountno;
+  }
+  
+  public static void main (String[]args){
+     Test t = new Test();
+  t.setAccountno(1234); // setting value by calling setter method 
+    
+   System.out.println(t.getAccountno());
+  
+  
+    
+  }
+ 
+  
+}
